d0b31b98f4d38b19ce4dd38eae7cdf56d25a58c1
Paweł Szulc <paul.szulc@gmail.com>
2016-01-24 13:03:19 +0100

    Refactor tests

--- src/test/scala/DimondKataTest.scala
+++ src/test/scala/DimondKataTest.scala
@@ -2,42 +2,44 @@
 import org.scalacheck.Gen
 import org.scalatest._
 import org.scalatest.prop._
-import DimondKata._
 
 class DimondKataTest extends FunSuite with Matchers with PropertyChecks {
 
-  val char = Gen.alphaUpperChar
+  val dimonds = for {
+    char <- Gen.alphaUpperChar
+  } yield (char, DimondKata.dimond(char))
+
 
   def charIdx(c: Char) = c.toInt - 'A'.toInt
 
   test("property: dimond's number of lines equal to twice char index + 1") {
-    forAll(char) { ch =>
-      dimond(ch).size should equal((charIdx(ch) * 2) + 1)
+    forAll(dimonds) { case (char, dimond) =>
+      dimond.size should equal((charIdx(char) * 2) + 1)
     }
   }
 
   test("property: dimond has a non-zero number of lines") {
-    forAll(char) { ch =>
-      dimond(ch).size should not(equal(0))
+    forAll(dimonds) { case (char, dimond) =>
+      dimond.size should not(equal(0))
     }
   }
 
   test("property: all dimond's lines are of the same size") {
-    forAll(char) { ch =>
-      val d = dimond(ch)
-      d.foreach { line =>
-        line.size should equal(d(0).size)
+    forAll(dimonds) { case (char, dimond) =>
+      dimond.foreach { line =>
+        line.size should equal(dimond(0).size)
       }
     }
   }
 
   test("property: dimond's single line size is equal dimond's number of lines (square)") {
-    forAll(char) { ch =>
-      val d = dimond(ch)
-      d.foreach { line =>
-        d(0).size should equal(d.size)
+    forAll(dimonds) { case (char, dimond) =>
+      dimond.foreach { line =>
+        line.size should equal(dimond.size)
       }
     }
   }
+
+
 
 }
