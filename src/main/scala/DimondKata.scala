79270bc21f971e1e2fd72741ea8d8a6660a896f8
Paweł Szulc <paul.szulc@gmail.com>
2016-01-24 20:57:50 +0100

    Modify the refactored tests
    Previous refactor was messing up messages when tests were failing,
    because ScalaCheck tries to simplify the arguments provided to 'forAll'
    once the error occurs (providing the simpliest failing example). This
    refactor overcomes that problem.

--- src/main/scala/DimondKata.scala
+++ src/main/scala/DimondKata.scala
@@ -1,8 +1,8 @@
 object DimondKata {
 
-  type Line = List[String]
+  type Line = List[Char]
 
-  def dimond(c: Char): List[Line] = (1 to numberOfLines(c)).toList.map(number => (1 to numberOfLines(c)).toList.map(_.toString))
+  def dimond(c: Char): List[Line] = (1 to numberOfLines(c)).toList.map(number => (1 to numberOfLines(c)).toList.map(_.toString.toCharArray()(0)))
 
   private   def numberOfLines(c: Char) = (2 * (c.toInt - 'A'.toInt) + 1)
 
--- src/test/scala/DimondKataTest.scala
+++ src/test/scala/DimondKataTest.scala
@@ -2,30 +2,28 @@
 import org.scalacheck.Gen
 import org.scalatest._
 import org.scalatest.prop._
+import DimondKata.Line
 
 class DimondKataTest extends FunSuite with Matchers with PropertyChecks {
 
-  val dimonds = for {
-    char <- Gen.alphaUpperChar
-  } yield (char, DimondKata.dimond(char))
-
+  val chars = Gen.alphaUpperChar
 
   def charIdx(c: Char) = c.toInt - 'A'.toInt
 
   test("property: dimond's number of lines equal to twice char index + 1") {
-    forAll(dimonds) { case (char, dimond) =>
+    property { case (char, dimond) =>
       dimond.size should equal((charIdx(char) * 2) + 1)
     }
   }
 
   test("property: dimond has a non-zero number of lines") {
-    forAll(dimonds) { case (char, dimond) =>
+    property { case (char, dimond) =>
       dimond.size should not(equal(0))
     }
   }
 
   test("property: all dimond's lines are of the same size") {
-    forAll(dimonds) { case (char, dimond) =>
+    property { case (char, dimond) =>
       dimond.foreach { line =>
         line.size should equal(dimond(0).size)
       }
@@ -33,13 +31,20 @@ class DimondKataTest extends FunSuite with Matchers with PropertyChecks {
   }
 
   test("property: dimond's single line size is equal dimond's number of lines (square)") {
-    forAll(dimonds) { case (char, dimond) =>
+    property { case (char, dimond) =>
       dimond.foreach { line =>
         line.size should equal(dimond.size)
       }
     }
   }
 
+  
+
+  def property(check: (Char, List[Line]) => Unit) = {
+    forAll(chars) { char =>
+      check(char, DimondKata.dimond(char))
+    }
+  }
 
 
 }
